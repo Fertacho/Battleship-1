919849683a4ab078e1f97b469ea47937a5247a2a
Felipe Muñoz <felipe.munoz.1997@gmail.com>
2023-01-21 15:35:57 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,107 +1,74 @@
-/* eslint-disable */
-import "./style.css";
+import React, { useState } from "react";
 
-//window.onload = function() {
-//write your code here
-// console.log("Hello Rigo from the console!");
-//};
-
-let myGridSize = prompt("¿De qué tamaño quieres tu tablero?");
-let enemyGridSize = prompt("¿De qué tamaño quieres el tablero enemigo?");
-let myGrid = createGrid(myGridSize);
-let enemyGrid = createGrid(enemyGridSize);
-let myShips = 3;
-let enemyShips = 3;
-let enemyLocations = {};
-
-printGrid(enemyGrid, true);
-printGrid(myGrid);
-
-for (let i = 1; i < 4; i++) {
-  let x = prompt("Ingresa las coordenadas de tu barco" + i);
-  let y = prompt("Ingresa las coordenadas de tu barco" + i);
-  placeCharacter(x, y, "O", myGrid);
-  placeRandomCharacter("O", enemyGrid, enemyGridSize);
-}
-
-printGrid(enemyGrid, true);
-printGrid(myGrid);
-
-while (enemyShips > 0 && myShips > 0) {
-  let x = prompt("ingresa la coordenada x para tu ataque");
-  let y = prompt("ingresa la coordenada y para tu ataque");
-
-  if (attack(x, y, enemyGrid)) {
-    enemyShips--;
+// create a function to generate a random game board
+function generateBoard() {
+  // create an empty board
+  const board = [];
+  for (let i = 0; i < 10; i++) {
+    board.push(new Array(10).fill(null));
   }
 
-  printGrid(enemyGrid, true);
-  printGrid(myGrid);
+  // add a random ship to the board
+  const shipRow = Math.floor(Math.random() * 10);
+  const shipCol = Math.floor(Math.random() * 10);
+  board[shipRow][shipCol] = "ship";
+
+  return board;
 }
 
-function createGrid(size) {
-  let grid = [];
-  for (let i = 0; i < size; i++) {
-    grid[i] = [];
-    for (let j = 0; j < size; j++) {
-      grid[i][j] = "-";
+function GameBoard() {
+  // generate a random game board on initial render
+  const [board, setBoard] = useState(generateBoard());
+
+  // define a function to handle cell clicks
+  function handleClick(row, col) {
+    // create a copy of the current board
+    const newBoard = board.slice();
+
+    // if the cell at the specified row and column has a ship, mark it as hit
+    if (board[row][col] === "ship") {
+      newBoard[row][col] = "hit";
+    } else {
+      // otherwise, mark it as a miss
+      newBoard[row][col] = "miss";
     }
+
+    // update the board state with the new board
+    setBoard(newBoard);
   }
-  return grid;
+
+  // render the game board
+  return (
+    <div>
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex}>
+          {row.map((cell, colIndex) => (
+            <div key={colIndex} onClick={() => handleClick(rowIndex, colIndex)}>
+              <Ship cell={cell} />
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
-function printGrid(grid, isEnemy = false) {
-  const headers = createHeaders(grid.length);
-  console.log(headers);
-  for (let i = 0; i < grid.length; i++) {
-    let rowStr = i + "  ";
-    for (let cell of grid[i]) {
-      if (isEnemy && cell == "O") {
-        rowStr += "- ";
-      } else {
-        rowStr += cell + "  ";
-      }
-    }
-    console.log(rowStr);
+function Ship({ cell }) {
+  // if the cell contains a ship, render a ship
+  if (cell === "ship") {
+    return <div className="ship">Ship</div>;
   }
-}
 
-function createHeaders(size) {
-  let result = "  ";
-
-  for (let i = 0; i < size; i++) {
-    result += i + " ";
+  // if the cell contains a hit, render a hit
+  if (cell === "hit") {
+    return <div className="hit">Hit</div>;
   }
-  return result;
-}
 
-function placeCharacter(x, y, c, grid) {
-  grid[y][x] = c;
-}
-
-function placeRandomCharacter(c, grid, max) {
-  let didPlace = false;
-  while (!didPlace) {
-    let x = getRandomInt(max);
-    let y = getRandomInt(max);
-    if (!enemyLocations["${x}-${y}"]) placeCharacter(x, y, c, grid);
-    didPlace = true;
-    enemyLocations["${x}-${y}"] = true;
+  // if the cell contains a miss, render a miss
+  if (cell === "miss") {
+    return <div className="miss">Miss</div>;
   }
-}
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function attack(x, y, grid) {
-  if (grid[y][x] == "O") {
-    grid[y][x] == "!";
-    return true;
-  } else if (grid[y][x] == "-") {
-    grid[y][x] = "x";
-    return false;
-  } else {
-    return false;
-  }
+  // otherwise, render an empty cell
+  return <div className="empty">Empty</div>;
 }
